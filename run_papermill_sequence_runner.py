ee5c7f50f987c55cd279bbe0a719f53e2479b4cd
Md Shahriar Forhad <89806405+Shahriar88@users.noreply.github.com>
2025-11-02 22:30:33 -0600

    Update run_papermill_sequence_runner.py

--- run_papermill_sequence_runner.py
+++ run_papermill_sequence_runner.py
@@ -6,6 +6,18 @@ Created on Sun Nov  2 21:46:22 2025
 
 ❌ Papermill only works with Jupyter notebooks (.ipynb), not plain Python .py scripts.
 
+If you want to input arguments then add first code cell like this:
+
+# Parameters
+run_name = "default_run"
+batch_size = 8
+epochs = 100
+scheduler_option = 4
+learning_rate = 1e-3
+
+
+
+
 pip install papermill
 python run_papermill_sequence_runner.py
 
@@ -25,8 +37,9 @@ NOTEBOOKS = [
 output_dir = Path("executed_notebooks")
 output_dir.mkdir(exist_ok=True)
 
-print(f"=== Run started {datetime.now():%Y-%m-%d %H:%M:%S} ===")
 
+
+print(f"=== Run started {datetime.now():%Y-%m-%d %H:%M:%S} ===")
 for nb in NOTEBOOKS:
     input_path = Path(nb)
     output_path = output_dir / input_path.name.replace(".ipynb", f"-executed.ipynb")
@@ -40,6 +53,37 @@ for nb in NOTEBOOKS:
         progress_bar=True
     )
     print(f"✅ Done: {output_path}")
-
 print(f"\n=== All notebooks completed at {datetime.now():%Y-%m-%d %H:%M:%S} ===")
+
+
+
+
+# Example — passing different params per notebook *************************************************
+NOTEBOOKS = {
+    "V1.0_Lipschitz_3ch.ipynb": {"run_name": "Lipschitz_3ch", "in_ch": 3},
+    "V1.0_Lipschitz_6ch.ipynb": {"run_name": "Lipschitz_6ch", "in_ch": 6},
+    "V1.0_RedPlat_3ch.ipynb": {"run_name": "RedPlat_3ch", "in_ch": 3},
+    "V1.0_RedPlat_6ch.ipynb": {"run_name": "RedPlat_6ch", "in_ch": 6},
+}
+
+print(f"=== Run started {datetime.now():%Y-%m-%d %H:%M:%S} ===")
+for nb, params in NOTEBOOKS.items():
+    input_path = Path(nb)
+    output_path = output_dir / input_path.name.replace(".ipynb", "-executed.ipynb")
+
+    print(f"\n▶ Running {input_path.name} ...")
+    pm.execute_notebook(
+        input_path,
+        output_path,
+        parameters=params,
+        kernel_name=None, # or "python3"
+        progress_bar=True
+    )
+    print(f"✅ Done: {output_path}")
+print(f"\n=== All notebooks completed at {datetime.now():%Y-%m-%d %H:%M:%S} ===")
+
+
+
+
+
 
